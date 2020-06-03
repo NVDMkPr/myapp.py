3844c0154916b3e7e6e8266472245242d6d92d48
NVDMkPr <55516796+NVDMkPr@users.noreply.github.com>
2020-06-03 19:54:50 +0700

    myapp.py

---
+++ myapp.py
@@ -0,0 +1,21 @@
+#Imports project list
+import asyncio
+import logging
+import re
+import time
+import os
+import sys
+import requests
+
+#Configs logging
+logging.basicConfig(level=logging.ERROR)
+
+#Imports list
+from telethon.tl.types import UpdateShortMessage,ReplyInlineMarkup,KeyboardButtonUrl
+from telethon import TelegramClient, events
+from telethon.tl.functions.channels import JoinChannelRequest
+from telethon.tl.functions.messages import GetBotCallbackAnswerRequest
+from datetime import datetime
+from bs4 import BeautifulSoup
+
+#Clear Text On Terminal
