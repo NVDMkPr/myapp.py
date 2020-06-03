4a0d7c0a6b299923bd9afe1dc37d154959994ff7
NVDMkPr <55516796+NVDMkPr@users.noreply.github.com>
2020-06-03 20:15:16 +0700

    myapp.py

--- myapp.py
+++ myapp.py
@@ -6,10 +6,10 @@ import time
 import os
 import sys
 import requests
-
+#-----
 #Configs logging
 logging.basicConfig(level=logging.ERROR)
-
+#-----
 #Imports list
 from telethon.tl.types import UpdateShortMessage,ReplyInlineMarkup,KeyboardButtonUrl
 from telethon import TelegramClient, events
@@ -17,5 +17,12 @@ from telethon.tl.functions.channels import JoinChannelRequest
 from telethon.tl.functions.messages import GetBotCallbackAnswerRequest
 from datetime import datetime
 from bs4 import BeautifulSoup
-
+#-----
 #Clear Text On Terminal
+os.system('clear')
+#-----
+#Open file phone numbers
+_openFile = open("SDT.txt","r")
+  #
+  #Read phone numbers from list file
+  println(_openFile.readlines(0))
