1dd1f8b7383f6f277aa4b033803600ed596cd038
NVDMkPr <55516796+NVDMkPr@users.noreply.github.com>
2020-06-04 10:51:50 +0700

    myapp.py

--- myapp.py
+++ myapp.py
@@ -1,5 +1,5 @@
 #All custom name variables will use char "_" in first digit
-#Imports project list
+#
 import asyncio
 import logging
 import re
@@ -8,10 +8,10 @@ import os
 import sys
 import requests
 #-----
-#Configs logging
+
 logging.basicConfig(level = logging.ERROR)
 #-----
-#Imports list
+
 from telethon.tl.types import UpdateShortMessage,ReplyInlineMarkup,KeyboardButtonUrl
 from telethon import TelegramClient, events
 from telethon.tl.functions.channels import JoinChannelRequest
@@ -20,10 +20,10 @@ from datetime import datetime
 from bs4 import BeautifulSoup
 from stringcolor import *
 #-----
-#Clear Text On Terminal
+
 os.system('clear')
 #-----
-#Function open file and get phone numbers
+
 def _getPhoneNum():
   global _index
   _index = 0
@@ -32,14 +32,25 @@ def _getPhoneNum():
   _getLen = len(_getAllPhoneNum)
   return int(_getAllPhoneNum[_index])
 #-----
-#Print color text
+
 _waring = "#fbfa70"
 _nofication = "#2dff3dd8"
 _error = "#ff3232ee"
-#Print messages time
+
 def timeNow(typeColor, msg):
-  _time = cs('[' + f'{datetime.now().strftime("%H:%M:%S") }' + f']','#66ddfadc')
+  _time = cs('[' + f'{datetime.now().strftime("%H:%M:%S")}' + ']','#66ddfadc')
   _msg = cs(msg, typeColor)
   print(_time + _msg)
 #-----
-#Login to telegram acccount
+
+_api_id = 1324372
+_api_hash = '9f02ddf1f9be32bb78ebc2b20c34f212'
+
+if not os.path.exists("session"):
+  os.mkdir("session")
+_phoneNum = _getPhoneNum()
+
+client = TelegramClient('session/' + f'{_phoneNum}', _api_id, _api_hash)
+client.start(_phoneNum)
+timeNow(_nofication,'Tài Khoản Hiện Tại : '+ f'{_phoneNum}')
+
